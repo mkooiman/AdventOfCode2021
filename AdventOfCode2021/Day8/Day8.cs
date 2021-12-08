3c860fc1c0de49c116bf4a01483ffd8d0323cc40
Michiel Kooiman <mkooiman@gmail.com>
2021-12-08 09:39:45 +0100

    Day8Pt2

--- AdventOfCode2021/Day8/Day8.cs
+++ AdventOfCode2021/Day8/Day8.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
@@ -24,9 +25,90 @@ namespace AdventOfCode2021.Day8
 
         }
         
+        public static void Day8Pt2()
+        {
+            var input = File.ReadAllLines(InputFile);
+
+
+            var result = input
+                .Select(x =>
+                    x.Split(" | ")
+                        .Select(x =>x.Split(' '))
+                        .ToArray());
+            var total = 0;
+            foreach (var strings in result)
+            {
+                var mapping = GetMapping(strings[0]);
+                var value = 0;
+                foreach (var s in strings[1])
+                {
+                    value =  ( value * 10 ) + Array.IndexOf(mapping,s.Sort());
+                }
+
+                total += value;
+            }
+            Console.WriteLine($"Result: {total}");
+
+        }
+
+        
+        
+        private static string[] GetMapping(string[] input)
+        {
+            var one = input.SingleOrDefault(x => x.Length == 2);
+            var seven = input.SingleOrDefault(x => x.Length == 3);
+            var four = input.SingleOrDefault(x => x.Length == 4);
+            var eight = input.SingleOrDefault(x => x.Length == 7);
+            
+            var fivelen = input.Where(x => x.Length == 5).ToList();
+            if(fivelen.Count()!=3)
+                throw new ArgumentException("There should be exactly 3 strings of length 5");
+            
+            var sixlen = input.Where(x => x.Length == 6).ToList();
+            
+            if(sixlen.Count!= 3)
+                throw new ArgumentException("There should be exactly 3 six length digits");
+            
+            var three = fivelen
+                .FirstOrDefault(x => x.IndexOf(one[0]) != -1 && x.IndexOf(one[1]) != -1);
+            fivelen.Remove(three);
+            var six = sixlen
+                .FirstOrDefault(x => x.IndexOf(one[0])==-1 || x.IndexOf(one[1]) == -1);
+            sixlen.Remove(six);
+            
+            var tmp = six.Aggregate("abcdefg", (current, c) => current.Replace("" + c, ""));
+
+            var two = fivelen.SingleOrDefault(x => x.Contains(tmp));
+            fivelen.Remove(two);
+            var five = fivelen[0];
+            string zero = null;
+            string nine = null;
+            if (four.Any(c => !sixlen[0].Contains(c)))
+            {
+                zero = sixlen[0];
+                nine = sixlen[1];
+            }
+
+            if (zero == null)
+            {
+                nine = sixlen[0];
+                zero = sixlen[1];
+            }
+
+            return new []{ zero.Sort(), one.Sort(), two.Sort(), three.Sort(), four.Sort(), five.Sort(), 
+                six.Sort(), seven.Sort(), eight.Sort(), nine.Sort()};
+        }
+
         public static string[] Split(this string target, string separator)
         {
             return target.Split(new[] {separator}, StringSplitOptions.None);
+        }
+        
+        public static string Sort( this string target)
+        {
+            var result = target.ToCharArray();
+            Array.Sort(result);
+            return new string(result);
         }
     }
 }
