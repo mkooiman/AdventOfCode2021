cb6d3b06fd490c5affbdd25f02e21aee329c60af
Michiel Kooiman <mkooiman@gmail.com>
2021-12-14 12:28:38 +0100

    Clean up

--- AdventOfCode2021/Day14/Day14.cs
+++ AdventOfCode2021/Day14/Day14.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Diagnostics.Tracing;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -30,7 +29,7 @@ namespace AdventOfCode2021.Day14
             }
             
             var counts = CountCharacters(result);
-            var min = Int64.MaxValue;
+            var min = long.MaxValue;
             var max = 0L;
             foreach (var keyValuePair in counts)
             {
@@ -53,41 +52,16 @@ namespace AdventOfCode2021.Day14
                 .ToDictionary(k => k[0], v => v[1][0]);
             
 
-            var counts = dict.ToDictionary(k => k.Key, (_) => 0L);
             var charCounts = "abcdefghijklmnopqrstuvwxyz".ToUpper().ToDictionary(k => k, v => 0L);
-            var cycles = 40;
-            for( var i =1; i < start.Length; i++)
-            {
-                var tmp = start[i - 1] +""+ start[i];
-                
-                counts[tmp]++;
-            }
-
-            for (int i = 0; i < cycles; i++)
-            {
-                var newCount = dict.ToDictionary(k => k.Key, (_) => 0L);
-                foreach (var keyValuePair in counts)
-                {
-                    var result = dict[keyValuePair.Key];
-
-                    var one = keyValuePair.Key[0] + "" + result;
-                    var two = "" + result + keyValuePair.Key[1];
-                    newCount[one] += keyValuePair.Value;
-                    newCount[two] += keyValuePair.Value;
-                }
-    
-                counts = newCount;
-            }
-            
+            var counts = GetCounts(start, 40, dict);
             foreach (var keyValuePair in counts)
             {
-                
                 charCounts[keyValuePair.Key[0]] += keyValuePair.Value;
                 charCounts[keyValuePair.Key[1]] += keyValuePair.Value;
             }
             
             var min = long.MaxValue;
-            var max = 0l;
+            var max = 0L;
             foreach (var keyValuePair in charCounts)
             {
                 if (keyValuePair.Value > max)
@@ -98,6 +72,32 @@ namespace AdventOfCode2021.Day14
             
             Console.WriteLine((max - min)/2 + 1 );
         }
+
+        private static Dictionary<string, long> GetCounts(string start, int cycles, Dictionary<string,char> map)
+        {
+            
+            var counts = map.ToDictionary(k => k.Key, (_) => 0L);
+            for( var i =1; i < start.Length; i++)
+            {
+                var tmp = start[i - 1] +""+ start[i];
+                counts[tmp]++;
+            }
+            for (var i = 0; i < cycles; i++)
+            {
+                var newCount = map.ToDictionary(k => k.Key, (_) => 0L);
+                foreach (var keyValuePair in counts)
+                {
+                    var result = map[keyValuePair.Key];
+
+                    var one = keyValuePair.Key[0] + "" + result;
+                    var two = "" + result + keyValuePair.Key[1];
+                    newCount[one] += keyValuePair.Value;
+                    newCount[two] += keyValuePair.Value;
+                }
+                counts = newCount;
+            }
+            return counts;  
+        }
         
         private static Dictionary<char, long> CountCharacters(string polymer)
         {
@@ -106,13 +106,9 @@ namespace AdventOfCode2021.Day14
             foreach (var c in polymer)
             {
                 if (counts.ContainsKey(c))
-                {
                     counts[c]++;
-                }
                 else
-                {
-                    counts.Add(c, 1l);
-                }
+                    counts.Add(c, 1L);
             }
 
             return counts;
@@ -132,33 +128,6 @@ namespace AdventOfCode2021.Day14
             return sb.ToString();
         }
 
-        private static Dictionary<string, long> PolymerizedCount(string source, Dictionary<string,char> dict, int cycles)
-        {
-            var counts = dict.ToDictionary(k => k.Key, (_) => 0l);
-            for( var i =1; i < source.Length; i++)
-            {
-                var tmp = source[i - 1] +""+ source[i];
-                counts[tmp] = 1;
-            }
-            
-            for (var i = 0; i < cycles; i++)
-            {
-                var newCount = dict.ToDictionary(k => k.Key, (_) => 0l);
-                foreach (var keyValuePair in counts)
-                {
-                    var result = dict[keyValuePair.Key];
-                    var one = keyValuePair.Key[0] + "" + result;
-                    var two = "" + result + keyValuePair.Key[1];
-                    newCount[one] += keyValuePair.Value;
-                    newCount[two] += keyValuePair.Value;
-                }
-    
-                counts = newCount;
-            }
-
-            return counts;
-        }
-        
         private static string[] Split(this string target, string separator)
         {
             return target.Split(new[] {separator}, StringSplitOptions.None);
